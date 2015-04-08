2e7eeb17949900c657a0b986fcd691ce11175a1e
Paul Stinchcombe <paul.dragonsoft@gmail.com>
2015-04-08 15:43:46 +0800

    Version 1.0
    Updated to push the “Choose Launcher” picker.  Does not work on recent
    CKT builds.  Will modify the Kodak launcher to handle this for all CKT
    builds

--- src/com/bitlogica/kodak/kme/ResetDefaultHome.java
+++ src/com/bitlogica/kodak/kme/ResetDefaultHome.java
@@ -11,29 +11,39 @@ public class ResetDefaultHome extends Activity {
 	public ResetDefaultHome() { }
 
 	@Override
-	protected void onNewIntent(Intent intent) {
-		super.onNewIntent(intent);
-	}
-
-
-	@Override
 	protected void onResume() {
 		super.onResume();
-		//getApplicationContext().sendBroadcast(new Intent("LAUNCHER_ENABLE_SIMPLE_UI").addCategory("KODAK_UI"));
-
-	   	ComponentName componentName = new ComponentName(getApplicationContext(), FakeHome.class);
-        PackageManager pm = getApplicationContext().getPackageManager();
-        pm.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
-        Intent i=new Intent(Intent.ACTION_MAIN);
-        i.addCategory(Intent.CATEGORY_HOME);
-        i.putExtra("default", false);
-        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        getApplicationContext().startActivity(i);
-        pm.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
-		
-		Toast.makeText(getApplicationContext(), "Done", Toast.LENGTH_LONG).show();
+		getBaseContext().sendBroadcast(new Intent("LAUNCHER_ENABLE_SIMPLE_UI").addCategory("KODAK_UI"));
 		finish();
 	}
 
 
 }
+
+
+
+//final Intent intent=new Intent();
+//intent.setAction(Intent.ACTION_MAIN);
+//intent.addCategory(Intent.CATEGORY_HOME);
+//startActivity(intent);
+
+//PackageManager p = getPackageManager();
+//ComponentName cN = new ComponentName(getBaseContext(), FakeHome.class);
+//p.setComponentEnabledSetting(cN, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+//Intent selector = new Intent(Intent.ACTION_MAIN);
+//selector.addCategory(Intent.CATEGORY_HOME);            
+//startActivity(selector);
+//p.setComponentEnabledSetting(cN, PackageManager.COMPONENT_ENABLED_STATE_DISABLED,    PackageManager.DONT_KILL_APP);
+
+
+//ComponentName componentName = new ComponentName(getBaseContext(), FakeHome.class);
+//PackageManager pm = getPackageManager();
+//pm.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+//Intent i=new Intent(Intent.ACTION_MAIN);
+//i.addCategory(Intent.CATEGORY_HOME);
+//i.putExtra("default", false);
+//i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//getApplicationContext().startActivity(i);
+//pm.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+//
+//Toast.makeText(getApplicationContext(), "Done again", Toast.LENGTH_LONG).show();
